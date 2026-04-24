d109e8d2f31950434c52f8f411be9ddfeeb9ce94
Ahmed Elnazer <ahmedelnazer30@gmail.com>
2026-04-24 03:32:44 +0300

    ResponseEntity is added

--- src/main/java/com/elnazer/quizapp/controller/QuestionController.java
+++ src/main/java/com/elnazer/quizapp/controller/QuestionController.java
@@ -15,7 +15,7 @@ public class QuestionController {
     QuestionService questionService;
 
     @GetMapping("allQuestions")
-    public List<Question> getAllQuestions(){
+    public ResponseEntity<List<Question>> getAllQuestions(){
         return questionService.getAllQuestions();
     }
     @GetMapping("category/{category}")
